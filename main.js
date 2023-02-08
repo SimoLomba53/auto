5991b53cf1381a0d928f82fe8bd1e11d8571a98a
SimoLomba53 <simolomba253@gmail.com>
2023-02-08 12:47:40 +0100

    animals

--- main.js
+++ main.js
@@ -50,3 +50,28 @@ const rest = auto.filter((auto) => {
 console.log(benz);
 console.log(td);
 console.log(rest);
+
+const animals = [
+    { nome: "leone", famiglia: "felidi", classe: "mammiferi" },
+    { nome: "cane", famiglia: "canidi", classe: "mammiferi" },
+    { nome: "gallina", famiglia: "fasianidi", classe: "uccelli" },
+]
+
+const mammiferi=animals.filter((animals) => {
+    return animals.classe =="mammiferi";
+}
+)
+
+console.log(mammiferi);
+
+//STESSA FORMA DI SOPRA MA SENZA RETURN SCRITTA IN MODO DIVERSO
+const uccelli=animals.filter((animals)=>animals.classe == "uccelli");
+console.log(uccelli);
+
+const felidi=animals.filter((animals)=>{
+    return animals.famiglia=="felidi"
+}
+
+)
+
+console.log(felidi);
